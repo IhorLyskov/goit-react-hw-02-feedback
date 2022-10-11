4a823b601179a01cd1de40f5f23d1f6bfb8633a1
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-10-11 21:56:44 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Section from './Section/Section';
+import ButtonsDiv from './ButtonsDiv/ButtonsDiv';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+import { GlobalStyle } from './GlobalStyle/GlobalStyle.styled';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  incrementActionState = action => {
+    this.setState(prevState => {
+      return { [action]: prevState[action] + 1 };
+    });
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage(total) {
+    return Math.round((this.state.good * 100) / total);
+  }
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage(total);
+
+    let statistics;
+    total
+      ? (statistics = (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ))
+      : (statistics = (
+          <Notification message="There is no feedback"></Notification>
+        ));
+
+    return (
+      <>
+        <GlobalStyle />
+        <Section title="Please live feedback">
+          <ButtonsDiv
+            actions={Object.keys(this.state)}
+            onClick={this.incrementActionState}
+          />
+        </Section>
+        <Section title="Statistics">{statistics}</Section>
+      </>
+    );
+  }
+}
